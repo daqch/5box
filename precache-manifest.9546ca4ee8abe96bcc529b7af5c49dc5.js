d28502665ba89cf212214df84df4da2c8796f55d
diego <diego_6525@hotmail.com>
2020-08-15 08:35:09 -0700

    Updates

---
+++ precache-manifest.9546ca4ee8abe96bcc529b7af5c49dc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e51eff5b1b0e2e9b860286d135b67d",
+    "url": "/5box/index.html"
+  },
+  {
+    "revision": "6ca82159e4d4010c7606",
+    "url": "/5box/static/css/main.824090c8.chunk.css"
+  },
+  {
+    "revision": "02095a4179162c3adc0f",
+    "url": "/5box/static/js/2.5ded5448.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/5box/static/js/2.5ded5448.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca82159e4d4010c7606",
+    "url": "/5box/static/js/main.e8f6a521.chunk.js"
+  },
+  {
+    "revision": "9e5e50ea3727173366ed",
+    "url": "/5box/static/js/runtime-main.22ca81ec.js"
+  }
+]);
